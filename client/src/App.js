93240b81a4d17f009fc2a4bae50be54358bffc9f
Jackson <jacksonfarren@hotmail.com>
2023-01-24 14:59:39 -0800

    completed apollo functionality in app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,26 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <Router>
